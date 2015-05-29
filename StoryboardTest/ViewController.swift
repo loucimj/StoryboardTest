845d14fe24fb4e500c91c1c786ee1e72620f6f0d
Javier <javier.loucim@gmail.com>
2015-05-29 16:31:45 -0300

    Initial Commit

---
+++ StoryboardTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StoryboardTest
+//
+//  Created by Javi on 29/5/15.
+//  Copyright (c) 2015 Javier Loucim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
